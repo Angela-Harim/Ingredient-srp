7e460daf6b150857f8f1aa457d726d2babd3dfcc
Angela-Haim <hei.angela.5@gmail.com>
2026-03-30 11:35:32 +0300

    first commit

---
+++ src/main/java/com/TD5/ingredient_srp/IngredientSrpApplication.java
@@ -0,0 +1,13 @@
+package com.TD5.ingredient_srp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredientSrpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngredientSrpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TD5/ingredient_srp/IngredientSrpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TD5.ingredient_srp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngredientSrpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
